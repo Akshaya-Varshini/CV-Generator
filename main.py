a9541b16deaa828947691663371d57e7a85d28f7
VarshiniDev <akshayavarshiniadev@gmail.com>
2024-08-16 21:28:28 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from tkinter import *
+
+
+def generate_cv_pdf():
+    name = entry_name.get()
+    email = entry_email.get()
+    phone_number = entry_phone.get()
+    address = entry_address.get()
+    website = entry_website.get()
+    skills = entry_skills.get("1.0", END).strip().split('\n')
+    work_experience_line = entry_experience.get("1.0", END).strip().split('\n')
+    for line in work_experience_line:
+        title, description = line.split(":")
+
+
+window = Tk()
+window.title("CV Generator")
+label_name = Label(window, text="Name : ")
+label_name.pack()
+entry_name = Entry(window)
+entry_name.pack()
+
+label_email = Label(window, text="Email : ")
+label_email.pack()
+entry_email = Entry(window)
+entry_email.pack()
+
+label_phone = Label(window, text="Phone : ")
+label_phone.pack()
+entry_phone = Entry(window)
+entry_phone.pack()
+
+label_address = Label(window, text="Address : ")
+label_address.pack()
+entry_address = Entry(window)
+entry_address.pack()
+
+label_website = Label(window, text="Website : ")
+label_website.pack()
+entry_website = Entry(window)
+entry_website.pack()
+
+label_skills = Label(window, text="Skills (Enter 1 skill per line)")
+label_skills.pack()
+entry_skills = Text(window, height=5)
+entry_skills.pack()
+
+label_education = Label(window, text="Education (One per line in format 'Degree' : 'University')")
+label_education.pack()
+entry_education = Text(window, height=5)
+entry_education.pack()
+
+label_experience = Label(window, text="Experience (One per line in format 'Job Title' : 'Job Description')")
+label_experience.pack()
+entry_experience = Text(window, height=5)
+entry_experience.pack()
+
+label_about_me = Label(window, text="About me")
+label_about_me.pack()
+entry_about_me = Text(window, height=5)
+entry_about_me.pack()
+
+button_generate = Button(window, text="Generate CV", command=generate_cv_pdf)
+button_generate.pack()
+
+window.mainloop()
